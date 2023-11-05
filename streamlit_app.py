8cf1823ecaf650d54ce97fb64a87b791f19a2c03
Todd-W-Davies <68192043+Todd-W-Davies@users.noreply.github.com>
2023-11-05 19:09:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,20 @@ import streamlit as st
 
 st.write("""
 # My first app
-Hello *world!*
+Animal Noises
 """)
+
+animal_dict = {
+  'cat': 'miaow',
+  'cow': 'moo',
+  'dog': 'woof',
+  'duck': 'quack,
+  'fish': 'blub',
+  'horse': 'neigh',
+  'pig': 'oink'
+}
+
+
+animal = st.selectbox(
+    'Select an animal',
+    list(animal_dict.keys())
